b77daff5305c433897995f68e9d5a89781c3eab4
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-16 11:14:34 +0900

    フォーム投稿によるイベント発火

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,5 +1,17 @@
 function post (){
-  console.log("非同期投稿を実装");
+  // console.log("非同期投稿を実装");（問題1で試しに記述）
+
+  // クリックされる送信ボタンの要素を取得
+  const submit = document.getElementById("submit_btn");
+  // クリックによるイベントには、clickを用意
+  submit.addEventListener("click", (e) => {
+    // フォームにはnew_articleというidが付与されているため、フォームの情報を取得
+    console.log(document.getElementById("new_article"));
+    // このままだとブラウザ上に用意されているデフォルトの送信が機能してしまい、重複してしまう
+    // デフォルトの送信をキャンセル
+    e.preventDefault();
+  });
+
 }
 
 
