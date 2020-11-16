8fae89350b994aed91bbfd7c779de9e2d94826a8
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-16 12:10:15 +0900

    イベント発火を定義

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,5 +1,13 @@
 function count (){
-  console.log("文字カウントを実装");
+  // console.log("文字カウントを実装");（問題1で試しに記述）
+
+  // 文字を入力する要素を取得
+  const articleText  = document.getElementById("article_text");
+  // その要素に何らかのキーボード操作が↓あった場合に、イベントが発火するように
+  articleText.addEventListener("keyup", () => {
+    // 取得した要素に含まれている値を出力
+    console.log(articleText.value);
+  });
 
 
 }
