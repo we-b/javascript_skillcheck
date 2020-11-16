a75f1df5b2f7bdc0be6db044954f92a4d9cf3eb7
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-16 12:21:47 +0900

    カウントした文字数を表示するように実装
    innerHTML

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -8,7 +8,14 @@ function count (){
     // 取得した要素に含まれている値を出力
     // console.log(articleText.value);（問題2で試しに記述）
     // 文字の数を数えるためには、lengthメソッドで文字数を出力
-    console.log(articleText.value.length);
+    // console.log(articleText.value.length);（問題3で試しに記述）
+
+    // 入力されている文字数をカウントした結果を取得
+    const countVal = articleText.value.length;
+    // 実際に文字を置き換えたい部分の要素（char_numというid↓が付与）を取得
+    const charNum  = document.getElementById("char_num");
+    // 取得した要素に対して、innerHTMLを用いて「0文字」と表示されている部分を置き換える
+    charNum.innerHTML = `${countVal}文字`;
   });
 
 
