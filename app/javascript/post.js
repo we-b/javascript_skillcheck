223e86c55d2458ac1b12fb841c148c66e4ccc5b8
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-16 11:22:38 +0900

    サーバーサイドにリクエスト送信

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -6,7 +6,22 @@ function post (){
   // クリックによるイベントには、clickを用意
   submit.addEventListener("click", (e) => {
     // フォームにはnew_articleというidが付与されているため、フォームの情報を取得
-    console.log(document.getElementById("new_article"));
+    // console.log(document.getElementById("new_article"));
+
+    // フォームの情報を取得し、Ajaxで送信できる形へと整形
+    const formData = new FormData(document.getElementById("new_article"));
+    //エンドポイントを呼び出すためにまず、オブジェクトを生成
+    const XHR = new XMLHttpRequest();
+    //どのようなリクエストをするのかを指定する（情報を初期化し、エンドポイントなどを設定）
+    XHR.open("POST", "/articles", true);
+    //レスポンスとして欲しい情報の形式を指定
+    XHR.responseType = "json";
+    // Ajaxで送信
+    XHR.send();
+
+    //『↑ここまでがリクエストの送信までの処理』
+
+    
     // このままだとブラウザ上に用意されているデフォルトの送信が機能してしまい、重複してしまう
     // デフォルトの送信をキャンセル
     e.preventDefault();
