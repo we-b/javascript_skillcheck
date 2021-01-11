b85a9e3ac372635157ade038edb9a376535edc1e
iwasatat0107 <it.was07@gmail.com>
2021-01-12 05:59:04 +0900

    サーバーサイドにリクエスト送信

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,7 +1,11 @@
 function post (){
   const submit = document.getElementById("submit_btn");
   submit.addEventListener("click", (e) => {
-
+    const formData = new FormData(document.getElementById("new_article"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/articles", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
     e.preventDefault();
   });
 }
