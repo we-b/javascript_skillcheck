6660b7714e68d22bbf1c55fd29848a2bfc15061c
iwasatat0107 <it.was07@gmail.com>
2021-01-12 06:29:25 +0900

    エラーを表示

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -7,7 +7,19 @@ function post (){
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-     
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const item = XHR.response.articles;
+      const contentArea = document.getElementById("contents_area");
+      const articleText = document.getElementById("article_text");
+      const HTML = `
+        <div class="article">
+        ${ item.text }
+        </div>`;
+      contentsArea.insertAdjacentHTML("afterbegin", HTML);
+      articleText.value = "";
     };
     e.preventDefault();
   });
