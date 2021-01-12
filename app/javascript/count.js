5e8ee7f05a4d86cd18494a33fbb1738be3799116
iwasatat0107 <it.was07@gmail.com>
2021-01-13 06:10:56 +0900

    イベント発火

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,4 +1,7 @@
-function count (){  
-
+function count (){
+  const articleText = document.getElementById("article_text");
+  articleText.addEventListener("keyup", () => {
+    
+  });
 }
 window.addEventListener('load', count);
