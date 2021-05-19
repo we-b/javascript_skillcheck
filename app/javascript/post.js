1c2f867bae72c73742bb4a89abb9f8c8475507cc
Kz-m <52796744+Kz-m@users.noreply.github.com>
2021-05-20 06:03:07 +0900

    イベントを発火させる
    フォームの投稿ボタンをクリックしたら、投稿したフォームの情報がコンソールに表示されるように実装
    
    クリックしたことを認識するためには、まずはクリックされる送信ボタンの要素を取得する必要があります。送信ボタンにはsubmit_btnというidが付与されているため、getElementByIdを用いて取得を行います。
    
    クリックによるイベントには、clickを用意します。そして、実際に行う処理を記述します。
    
    コンソール上に表示するのは、フォームの情報です。フォームにはnew_articleというidが付与されているため、getElementByIdを用いて取得します。
    
    また、このままだとブラウザ上に用意されているデフォルトの送信が機能してしまい、重複してしまいます。したがって、e.preventDefault();を用いてデフォルトの送信をキャンセルします。
    
    投稿したフォームの情報を確認できたら、console.log(document.getElementById("new_article"));は削除しておきましょう。

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,5 +1,9 @@
 function post(){
-  console.log("非同期投稿を実装");
+  const submit = document.getElementById("submit_btn") //ブラウザelementsの<input type="submit"~>のidの部分を見て、getElementByIdで取得
+  submit.addEventListener("click", (e) => {
+    // console.log(document.getElementById("new_article")); //投稿したフォームの情報がコンソールに表示される(この"new_article"はブラウザelementsの<form id="new_article"〜>を見て記述。コンソールに表示されることを確認したら削除
+    e.preventDefault(); // ブラウザ上に用意されているデフォの送信をキャンセルして、重複送信をさせない
+  });
 }
 
 window.addEventListener('load', post);
